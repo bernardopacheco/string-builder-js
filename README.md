d116128fa5dc7b7c566b699f9f4935d059e654ef
bernardopacheco <bernardo@bernardopacheco.net>
2020-04-08 22:22:29 -0700

    Set up project
    - babel
    - lint
    - jest

---
+++ README.md
@@ -0,0 +1,17 @@
+# String Builder JS
+
+## Usage
+
+## Download
+
+## Running tests
+
+**Install dependencies**
+```
+npm install
+```
+
+**Run tests**
+```
+npm test
+```
---
+++ src/string-builder.test.js
@@ -0,0 +1,5 @@
+import StringBuilderJS from "./string-builder";
+
+test("should get a string builder instance", () => {
+  expect(new StringBuilderJS()).toBeDefined();
+});
